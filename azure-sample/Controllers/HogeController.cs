760353e68528fc35044911068b385501bccf7874
n_morioka <n.morioka0@gmail.com>
2016-10-20 18:54:27 +0900

    add blob model

--- azure-sample/Controllers/HogeController.cs
+++ azure-sample/Controllers/HogeController.cs
@@ -16,6 +16,8 @@ namespace azure_sample.Controllers
     using System.Linq;
     using System.Threading.Tasks;
 
+    using azure_sample.Models;
+
     public class HogeController : ApiController
     {
         // GET api/hoge
@@ -23,37 +25,8 @@ namespace azure_sample.Controllers
         {
             const string ImageToUpload = "c:\\hoge.png";
 
-            // Retrieve storage account information from connection string
-            // How to create a storage connection string - http://msdn.microsoft.com/en-us/library/azure/ee758697.aspx
-            CloudStorageAccount storageAccount = CreateStorageAccountFromConnectionString(Microsoft.Azure.CloudConfigurationManager.GetSetting("StorageConnectionString"));
-
-            // Create a blob client for interacting with the blob service.
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-            // Create a container for organizing blobs within the storage account.
-            Console.WriteLine("1. Creating Container");
-            CloudBlobContainer container = blobClient.GetContainerReference("democontainerblockblob");
-            try
-            {
-                container.CreateIfNotExists();
-            }
-            catch (StorageException)
-            {
-                Console.WriteLine("If you are running with the default configuration please make sure you have started the storage emulator. Press the Windows key and type Azure Storage to select and run it from the list of applications - then restart the sample.");
-                Console.ReadLine();
-                throw;
-            }
-
-            // To view the uploaded blob in a browser, you have two options. The first option is to use a Shared Access Signature (SAS) token to delegate 
-            // access to the resource. See the documentation links at the top for more information on SAS. The second approach is to set permissions 
-            // to allow public access to blobs in this container. Uncomment the line below to use this approach. Then you can view the image 
-            // using: https://[InsertYourStorageAccountNameHere].blob.core.windows.net/democontainer/HelloWorld.png
-            // await container.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
-
-            // Upload a BlockBlob to the newly created container
-            Console.WriteLine("2. Uploading BlockBlob");
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference(ImageToUpload);
-            blockBlob.UploadFromFile(ImageToUpload);
+            BlobModel blobModel = new BlobModel();
+            blobModel.hoge(ImageToUpload);
 
             return new string[] { "hoge1", "hoge2" };
         }
@@ -83,7 +56,7 @@ namespace azure_sample.Controllers
                 fileName = Path.GetFileName(fileName);
 
                 // ファイルの移動
-                File.Move(file.LocalFileName, Path.Combine("C:\\", fileName));
+                File.Move(file.LocalFileName, Path.Combine("C:\\temp\\", fileName));
             }
 
             return this.Request.CreateResponse(HttpStatusCode.OK);
@@ -99,27 +72,5 @@ namespace azure_sample.Controllers
         {
         }
 
-        private CloudStorageAccount CreateStorageAccountFromConnectionString(string storageConnectionString)
-        {
-            CloudStorageAccount storageAccount;
-            try
-            {
-                storageAccount = CloudStorageAccount.Parse(storageConnectionString);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Invalid storage account information provided. Please confirm the AccountName and AccountKey are valid in the app.config file - then restart the sample.");
-                Console.ReadLine();
-                throw;
-            }
-            catch (ArgumentException)
-            {
-                Console.WriteLine("Invalid storage account information provided. Please confirm the AccountName and AccountKey are valid in the app.config file - then restart the sample.");
-                Console.ReadLine();
-                throw;
-            }
-
-            return storageAccount;
-        }
     }
 }
