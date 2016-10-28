a1034dc837e449365b100d692627f063b1aa2819
n_morioka <n.morioka0@gmail.com>
2016-10-28 18:53:09 +0900

    add worker

---
+++ azure-sample-worker/Utils/ImageProcessor.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+
+namespace Utils
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.IO;
+    using System.Collections.Generic;
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Auth;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using Microsoft.WindowsAzure.Storage.RetryPolicies;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+
+    public class ImageProcessor
+    {
+        private static int num = 0;
+        private static void execute(Stream input)
+        {
+            string root = RoleEnvironment.GetLocalResource("LocalStorage").RootPath;
+            string srcFilePath = "";
+            string dstFilePath = "";
+
+//            lock (thisLock)
+//            {
+
+                srcFilePath = root + @"Src\" + num + ".bmp";
+                dstFilePath = root + @"Dst\" + num + ".png";
+                ++num;
+
+            //            }
+
+            /*
+             ファイルの上限値はWeb.configの「maxRequestLength」で調整します。単位はKB
+            <system.web>
+                <compilation debug="true" targetFramework="4.5" />
+                <httpRuntime targetFramework="4.5" maxRequestLength="10240" />
+            </system.web>
+            */
+
+            if (input != null)
+            {
+                Bitmap bmp = new Bitmap(input);
+
+                int width = bmp.Width;
+                int height = bmp.Height;
+
+                if (width > height)
+                {
+                    double aspect = (double)width / height;
+
+                    height = 1080;
+                    width = (int)(1080 * aspect);
+
+                    if (width % 2 != 0)
+                    {
+                        --width;
+                    }
+                }
+                else
+                {
+                    double aspect = (double)height / width;
+
+                    width = 1080;
+                    height = (int)(1080 * aspect);
+
+                    if (height % 2 != 0)
+                    {
+                        --height;
+                    }
+                }
+                Bitmap resizeBmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                Graphics g = Graphics.FromImage(resizeBmp);
+
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bicubic;
+                g.DrawImage(bmp, 0, 0, width, height);
+
+                resizeBmp.Save(srcFilePath);
+
+//                ViewData["before_image1"] = ImageToBase64String(resizeBmp);
+//                ViewData["before_image1_width"] = resizeBmp.Width / 2;
+//                ViewData["before_image1_height"] = resizeBmp.Height / 2;
+
+                g.Dispose();
+                bmp.Dispose();
+                resizeBmp.Dispose();
+            }
+            System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo();
+
+            psi.UseShellExecute = false;
+            psi.WorkingDirectory = root;
+            psi.FileName = root + "image.exe";
+            psi.Arguments = String.Format("{0} {1} {2}",
+                root + "exe.xml",
+                dstFilePath,
+                srcFilePath
+                );
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start(psi);
+            p.WaitForExit();
+
+            if (input != null)
+            {
+//                FileStream file = new FileStream(dstFilePath, FileMode.Open);
+//                Bitmap bmp = new Bitmap(file);
+
+//                ViewData["image1"] = ImageToBase64String(bmp);
+//                ViewData["image1_width"] = bmp.Width / 2;
+//                ViewData["image1_height"] = bmp.Height / 2;
+
+//                bmp.Dispose();
+//                file.Close();
+            }
+
+            System.IO.File.Delete(srcFilePath);
+            System.IO.File.Delete(dstFilePath);
+        }
+    }
+}
---
+++ azure-sample-worker/Utils/Storage.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Utils
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.IO;
+    using System.Collections.Generic;
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Auth;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using Microsoft.WindowsAzure.Storage.RetryPolicies;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+
+    public class Storage
+    {
+        private static bool DownLoadDirectory(IEnumerable<IListBlobItem> list, string DLDir)
+        {
+            try
+            {
+                Directory.CreateDirectory(DLDir);
+
+                // コンテンツのダウンロード
+                foreach (IListBlobItem item in list)
+                {
+                    if (item.GetType() == typeof(CloudBlockBlob))
+                    {
+                        CloudBlockBlob blob = (CloudBlockBlob)item;
+
+                        blob.DownloadToFile(DLDir + Path.GetFileName(item.Uri.ToString()), FileMode.Create);
+                    }
+                    else if (item.GetType() == typeof(CloudPageBlob))
+                    {
+                        CloudPageBlob pageBlob = (CloudPageBlob)item;
+                    }
+                    else if (item.GetType() == typeof(CloudBlobDirectory))
+                    {
+                        CloudBlobDirectory directory = (CloudBlobDirectory)item;
+                        string prefix = directory.Prefix.Substring(0, directory.Prefix.Length - 1);
+
+                        int index = prefix.LastIndexOf("/");
+
+                        if (index >= 0)
+                        {
+                            prefix = prefix.Substring(index + 1);
+                        }
+                        DownLoadDirectory(directory.ListBlobs(), DLDir + prefix + @"\");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // 動画生成に必要なコンテンツをStorageからダウンロード
+        public static bool DownLoadContents()
+        {
+            try
+            {
+                StorageCredentials credentials =
+                    new StorageCredentials("cuticlestorage", "BXOaKxZovkSTcBGNNni32Rx5w2s1TrRPAMoIOI8DIBHynUKTXx2YtrDi/UcbsdC0toelXXm0J/dYelwHYJD9Pw==");
+                CloudStorageAccount account = new CloudStorageAccount(credentials, true);
+
+                // クライアントの作成
+                CloudBlobClient client = account.CreateCloudBlobClient();
+                CloudBlobContainer container = client.GetContainerReference("bin");
+
+                string root = RoleEnvironment.GetLocalResource("LocalStorage").RootPath;
+
+                DownLoadDirectory(container.ListBlobs(), root);
+
+                Directory.CreateDirectory(root + "Src");
+                Directory.CreateDirectory(root + "Dst");
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
